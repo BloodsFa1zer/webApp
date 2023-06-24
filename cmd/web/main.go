d7b4a27f91b4a28b3aa7b17b477e9bf78c5f5b9b
BloodsFa1zer <mixo228aga@gmail.com>
2023-06-24 09:31:14 +0300

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BloodsFa1zer/go-web-course/pkg/config"
+	"github.com/BloodsFa1zer/go-web-course/pkg/handlers"
+	"github.com/BloodsFa1zer/go-web-course/pkg/render"
+	"github.com/alexedwards/scs/v2"
+	"log"
+	"net/http"
+	"time"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+	serve := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = serve.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ pkg/render/render.go
@@ -0,0 +1,86 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/BloodsFa1zer/go-web-course/pkg/config"
+	"github.com/BloodsFa1zer/go-web-course/pkg/models"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string, templateData *models.TemplateData) {
+	var templateCache map[string]*template.Template
+	if app.UseCache {
+		// get the template cache from the app config
+		templateCache = app.TemplateCache
+	} else {
+		templateCache, _ = CreateTemplateCache()
+	}
+
+	t, ok := templateCache[tmpl] // ok will be true if the tmpl is exist
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	templateData = AddDefaultData(templateData)
+
+	_ = t.Execute(buf, templateData)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("error writing template to browser", err)
+	}
+
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		templateSet, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			templateSet, err = templateSet.ParseGlob("./templates/*.layout.tmpl.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = templateSet
+	}
+	return myCache, nil
+}
